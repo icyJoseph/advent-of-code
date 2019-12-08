6afe0ed402e16eecd105d607e56026a116a6118a
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-12-08 08:13:14 +0100

    refactor: remove unnecessary abstractions and shrink solution

--- js/dayeight.js
+++ js/dayeight.js
@@ -1,6 +1,60 @@
 const fs = require("fs");
 const path = require("path");
 
+const countDigits = arr =>
+  arr
+    .flat(Infinity)
+    .reduce(
+      (prev, curr) =>
+        prev[curr]
+          ? { ...prev, [curr]: prev[curr] + 1 }
+          : { ...prev, [curr]: 1 },
+      {}
+    );
+
+const compareZeroes = (a, b) => a[0] - b[0];
+
+const toStream = (pixels, size) =>
+  Array.from({ length: pixels.length / size }, (_, i) => i).map(layer =>
+    pixels.slice(layer * size, layer * size + size)
+  );
+
+const toLayers = (stream, width, height) =>
+  stream.reduce(
+    (prev, curr) => [
+      ...prev,
+      Array.from({ length: height }, (_, i) => i).map(row =>
+        curr.slice(width * row, width * row + width)
+      )
+    ],
+    []
+  );
+
+const makeEmptyCanvas = size => Array.from({ length: size }, (_, i) => i);
+
+// 0 is black, 1 is white, and 2 is transparent.
+const resolvePixel = (previousLayer, currentLayer) => {
+  switch (currentLayer) {
+    case "0":
+      return previousLayer === null ? "0" : previousLayer;
+    case "1":
+      return previousLayer === null ? "1" : previousLayer;
+    case "2":
+    default:
+      return previousLayer;
+  }
+};
+
+const prettyPrint = canvas => ([row, width]) => {
+  const pivot = row * width;
+  console.log(
+    canvas
+      .slice(pivot, pivot + width)
+      .map(x => (x === "0" ? " " : "*"))
+      .join("")
+  );
+};
+
 fs.readFile(
   path.resolve(__dirname, "../", "input/day_eight.in"),
   "utf-8",
@@ -11,75 +65,21 @@ fs.readFile(
     const height = 6;
     const size = width * height;
 
-    const pixels = data.split("");
+    const stream = toStream(data.split(""), size);
+    const layers = toLayers(stream, width, height);
 
-    const layerQty = pixels.length / size;
+    const [lowestZeroCount] = layers.map(countDigits).sort(compareZeroes);
 
-    const layers = Array.from({ length: layerQty }, (_, i) => i).map(layer =>
-      pixels.slice(layer * size, layer * size + size)
-    );
+    console.log(lowestZeroCount[1] * lowestZeroCount[2]);
 
-    const image = layers.reduce((prev, curr) => {
-      const pixelLayer = Array.from({ length: height }, (_, i) => i).map(
-        row => {
-          const ret = curr.slice(width * row, width * row + width);
-
-          return ret;
-        }
-      );
-      return [...prev, pixelLayer];
-    }, []);
-
-    let digitCount = [];
-
-    image.forEach(layer => {
-      const digitQty = layer.flat().reduce(
-        (prev, curr) => {
-          return prev[curr]
-            ? { ...prev, [curr]: prev[curr] + 1 }
-            : { ...prev, [curr]: 1 };
-        },
-        { 0: 0, 1: 0, 2: 0 }
-      );
-
-      digitCount.push(digitQty);
-    });
-
-    // 0 is black, 1 is white, and 2 is transparent.
-
-    const emptyCanvas = Array.from({ length: size }, (_, i) => i);
-
-    const result = emptyCanvas.map(index => {
+    const canvas = makeEmptyCanvas(size).map(index => {
       const row = Math.floor(index / width);
       const column = index % width;
-      const pixel = image
-        .map(layer => {
-          const ret = layer[row][column];
-          return ret;
-        })
-        .reduce((prev, curr) => {
-          switch (curr) {
-            case "0":
-              return prev === null ? "0" : prev;
-            case "1":
-              return prev === null ? "1" : prev;
-            case "2":
-            default:
-              return prev;
-          }
-        }, null);
-
-      return pixel;
+      return layers.map(layer => layer[row][column]).reduce(resolvePixel, null);
     });
 
-    Array.from({ length: height }, (_, i) => i).map(row => {
-      const pivot = width * row;
-      console.log(
-        result
-          .slice(pivot, pivot + width)
-          .map(x => (x === "0" ? " " : "*"))
-          .join("")
-      );
-    });
+    Array.from({ length: height }, (_, i) => [i, width]).forEach(
+      prettyPrint(canvas)
+    );
   }
 );
