86b7afa92444450a907bad4a3874aa4a0f836b0b
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-12-16 22:35:05 +0100

    feat: solve day 16 part two, wow

--- js/day_sixteen.js
+++ js/day_sixteen.js
@@ -4,59 +4,18 @@ const path = require("path");
 const oneDigit = num =>
   Math.abs(num) > 9 ? Math.abs(num) % 10 : Math.abs(num);
 
-const calcMultipler = (row, index) => {
-  const arc = 4 * (row + 1);
-  const diff = (index - row) % arc;
-  const coef = Math.floor(diff / (row + 1));
-
-  const next = coef === 0 || coef === 2 ? index + 2 * (row + 1) : index + 1;
-
-  switch (coef) {
-    case 0:
-      // return 1;
-      return { mult: 1, next };
-    case 3:
-    case 1:
-      // return 0;
-      return { mult: 0, next };
-    case 2:
-      // return -1;
-      return { mult: -1, next };
-    default:
-      throw new Error("Bad coef");
-  }
-};
-
-function fastSum(arr, from, to) {
-  let step = from;
-  let acc = 0;
-  while (step < to) {
-    acc = acc + (arr[step] || 0);
-    step = step + 1;
-  }
-  return acc;
-}
-
-function decode(size, output, skip) {
+function decode(output, skip) {
   let result = [];
-  let row = skip;
-  let sign = 1;
-  while (row < size) {
-    console.log({ row });
-    let index = row;
-    let sum = 0;
-    while (index < size) {
-      const { next } = calcMultipler(row, index);
-      sum =
-        sum + sign * fastSum(output, index, Math.min(index + row + 1, size));
-      sign = -1 * sign;
-      index = next;
-    }
-    result[row] = oneDigit(sum);
-    row = row + 1;
+  const size = output.length;
+  let row = size - 1;
+
+  while (row >= skip) {
+    result[row] = (result[row + 1] || 0) + output[row];
+
+    row = row - 1;
   }
 
-  return result;
+  return result.map(oneDigit);
 }
 
 fs.readFile(
@@ -66,9 +25,6 @@ fs.readFile(
     if (err) return console.log(err);
 
     const initialInput = data.split("").map(x => parseInt(x));
-
-    // const skip = 0;
-    // const repeats = 1;
 
     const skip = parseInt(initialInput.slice(0, 7).join(""));
     const repeats = 10000;
@@ -80,15 +36,12 @@ fs.readFile(
     let output = [...inputs];
     let phase = 0;
 
-    const iterations = output.length;
     while (phase < 100) {
-      console.log({ phase, total: iterations - skip });
-      output = decode(iterations, output, skip);
+      console.log({ phase });
+      output = decode(output, skip);
       phase = phase + 1;
     }
 
     console.log(output.slice(skip, skip + 8).join(""));
   }
 );
-
-// 73757050 too high
