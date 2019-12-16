f86abd30cb5fa051564be15ae71af214cb33bfa3
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-12-16 10:51:02 +0100

    chore: not working solution

--- js/day_sixteen.js
+++ js/day_sixteen.js
@@ -1,6 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
+function multiplyMatrices(m1, m2) {
+  var result = [];
+  for (var i = 0; i < m1.length; i++) {
+    result[i] = [];
+    for (var j = 0; j < m2[0].length; j++) {
+      var sum = BigInt(0);
+      for (var k = 0; k < m1[0].length; k++) {
+        sum += (m1[i][k] || BigInt(0)) * (m2[k][j] || BigInt(0));
+      }
+      result[i][j] = sum;
+    }
+  }
+  return result;
+}
+
 const oneDigit = num => (Math.abs(num) > 9 ? num % 10 : num);
 
 const slicer = iteration => step => {
@@ -9,15 +24,54 @@ const slicer = iteration => step => {
   return { from, to, multiplier: step % 2 === 0 ? 1 : -1 };
 };
 
+const insertIn = (arr, from, to, value) => {
+  let i = from;
+
+  while (1) {
+    arr[i] = value;
+    i = i + 1;
+    if (i >= to) {
+      break;
+    }
+  }
+};
+
+const generateMatrix = size => {
+  let pass = 0;
+  let rows = [];
+  // one pass for each row
+  while (pass < size) {
+    let k = 0;
+    // until we've filled the entire row
+    let row = [];
+    out: while (1) {
+      const fn = slicer(pass);
+
+      const { from, to, multiplier } = fn(k);
+
+      insertIn(row, from, to, BigInt(multiplier));
+      k = k + 1;
+      if (row.length > size) {
+        break out;
+      }
+    }
+
+    rows.push(row.slice(0, size));
+    // at the end of the pass, push the row
+    pass = pass + 1;
+  }
+  return rows;
+};
+
 const sum = (arr, from, to) => {
   acc = 0;
   let i = from;
 
-  while (i <= to) {
+  it: while (i <= to) {
     acc = acc + (arr[i] || 0);
     i = i + 1;
     if (i === arr.length) {
-      break;
+      break it;
     }
   }
   return acc;
@@ -43,18 +97,18 @@ const FFT = (outputs, iteration) => {
 };
 
 fs.readFile(
-  path.resolve(__dirname, "../", "input/day_sixteen.in"),
+  path.resolve(__dirname, "../", "input/example.in"),
   "utf-8",
   (err, data) => {
     if (err) return console.log(err);
 
     const initialInput = data.split("").map(x => parseInt(x));
 
-    // const skip = 0;
-    // const repeats = 1;
+    const skip = 0;
+    const repeats = 1;
 
-    const skip = parseInt(initialInput.slice(0, 8).join(""));
-    const repeats = 10000;
+    // const skip = parseInt(initialInput.slice(0, 8).join(""));
+    // const repeats = 10000;
 
     const inputs = Array.from({ length: repeats }, () => [
       ...initialInput
@@ -63,19 +117,25 @@ fs.readFile(
     let output = [...inputs];
     let phase = 0;
 
-    while (phase < 100) {
-      console.log("phase", phase);
-      const next = Array.from({ length: output.length }, (_, iteration) => {
-        // console.log("inner iteration", iteration, output.length, phase);
-        // these are independent
-        const fft = FFT(output, iteration);
-        return fft;
-      });
+    const iterations = output.length;
 
-      output = [...next];
+    let matrix = generateMatrix(iterations);
 
+    while (phase < 1) {
+      console.log({ phase });
+      matrix = multiplyMatrices(matrix, matrix);
       phase = phase + 1;
     }
+
+    let transform = [];
+
+    output.forEach((x, index) => {
+      transform[index] = [];
+      transform[index][0] = BigInt(x);
+    });
+
+    output = multiplyMatrices(matrix, transform);
+    console.log(output, matrix);
 
     console.log(output.slice(skip, skip + 8).join(""));
   }
