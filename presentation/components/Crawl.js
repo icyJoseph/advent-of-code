24d0030f36ff37bbd5d8a5212d5f71a260706994
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-02-12 17:01:49 +0100

    feat: introduce audio track

--- presentation/components/Crawl.js
+++ presentation/components/Crawl.js
@@ -1,37 +1,68 @@
 import React from "react";
+import Audio from "../assets/crawl.mp3";
 import "./style.css";
 
-export const Crawl = ({
-  useIntro = false,
-  title = "",
-  subtitle = "",
-  paragraphs = []
-}) => (
-  <>
-    {useIntro && (
-      <>
+export const Crawl = ({ title = "", subtitle = "", paragraphs = [] }) => {
+  const [start, setStart] = React.useState(false);
+  const [play, setPlay] = React.useState(false);
+  const _ref = React.useRef();
+
+  React.useEffect(() => {
+    const handler = () => {
+      setStart(true);
+    };
+
+    const el = _ref.current;
+
+    el.addEventListener("canplay", handler);
+
+    return () => {
+      el.removeEventListener("canplay", handler);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (start) {
+      setTimeout(() => {
+        _ref.current.play();
+        setPlay(true);
+      }, 7000);
+    }
+  }, [start]);
+
+  return (
+    <>
+      <audio preload="auto" ref={_ref}>
+        <source src={Audio} type="audio/mpeg" />
+      </audio>
+      {start && (
         <section className="intro">
           A long time ago, in a galaxy far,
           <br />
           far away...
         </section>
-        <section className="logo">
-          Advent of <br />
-          Code 2019
-        </section>
-      </>
-    )}
-    <section className="star-wars">
-      <div className="crawl">
-        <div className="title">
-          <p>{title}</p>
-          <h1>{subtitle}</h1>
-        </div>
+      )}
+      {play && (
+        <>
+          <section className="logo">
+            Advent of <br />
+            Code 2019
+          </section>
 
-        {paragraphs.map((text, index) => (
-          <p key={index}>{text}</p>
-        ))}
-      </div>
-    </section>
-  </>
-);
+          <section className="star-wars">
+            <div className="crawl">
+              <div className="title">
+                <p>{title}</p>
+                <h1>{subtitle}</h1>
+              </div>
+
+              {paragraphs.map((text, index) => (
+                <p key={index}>{text}</p>
+              ))}
+            </div>
+          </section>
+        </>
+      )}
+    </>
+  );
+};
