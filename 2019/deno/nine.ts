170ac952dd3e86e6670c524088e0520ed03f642a
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-11-18 12:57:06 +0100

    practice: rewrite problem 9 part two

--- 2019/deno/nine.ts
+++ 2019/deno/nine.ts
@@ -13,3 +13,11 @@ const memory = createMemory(program, 1);
 await memory.tick();
 
 console.log("Part One:", memory.readOutput());
+
+/**
+ * Part Two
+ */
+
+await createMemory(program, 2)
+  .tick()
+  .then((mem) => console.log("Part Two:", mem.readOutput()));
