72bbd7d125a343e79d8bc3ef86417ab5441f0f24
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-12-15 07:52:23 +0100

    feat: solve day 15 :star: :star:

---
+++ 2020/deno/fifteen.ts
@@ -0,0 +1,43 @@
+const input = await Deno.readTextFile("./input/fifteen.in").then((res) =>
+  res.split(",").map(Number)
+);
+
+/**
+ * Helpers
+ */
+
+const memoryGame = (limit: number) => {
+  const map = new Map<number, number[]>();
+
+  input.forEach((num, it) => map.set(num, [it + 1]));
+
+  let it = input.length + 1;
+  let [last] = input.slice(-1);
+
+  while (1) {
+    const [x1 = 0, x0 = x1]: number[] = map.get(last) ?? [];
+    const speak = x0 - x1;
+
+    map.set(speak, [...(map.get(speak) || []), it].slice(-2));
+
+    if (it === limit) return speak;
+
+    last = speak;
+    it = it + 1;
+  }
+};
+
+/**
+ * Part One
+ */
+
+console.log("Part One:", memoryGame(2020));
+
+/**
+ * Part Two
+ */
+
+console.time("2");
+console.log("Part Two:", memoryGame(30_000_000));
+
+console.timeEnd("2");
