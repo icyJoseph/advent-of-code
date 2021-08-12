a77da5a4c58aa7110ce904f99698fa73cb343ccb
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-08-12 10:19:31 +0200

    feat: Automated input from AoC

---
+++ aoc/src/bin/get_input.rs
@@ -0,0 +1,6 @@
+use aoc;
+
+fn main() {
+    aoc::get_input(2019, 1);
+    aoc::get_input(2020, 2);
+}
---
+++ aoc/src/lib.rs
@@ -0,0 +1,39 @@
+use dotenv;
+use reqwest;
+
+/// # get_input
+///
+/// Goes out to advent of code to get input for a given year and day.
+///
+/// It requires a valid session cookie.
+///
+/// Take the session cookie from https://adventofcode.com in your preferred
+/// browser. In Chrome this can be found in Application isnside `devTools`.
+///
+pub fn get_input(year: u32, day: u32) -> () {
+    dotenv::dotenv().ok();
+
+    let session = dotenv::var("SESSION");
+
+    match session {
+        Ok(value) => {
+            let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
+
+            let client = reqwest::blocking::Client::new();
+
+            let response = client
+                .get(url)
+                .header("Cookie", format!("session={}", value))
+                .send();
+
+            match response {
+                Ok(res) => match res.text() {
+                    Ok(txt) => println!("{}", txt),
+                    Err(_) => println!("Error reading response"),
+                },
+                Err(_) => println!("Failed to get"),
+            }
+        }
+        Err(_) => println!("Session cookie is missing"),
+    }
+}
