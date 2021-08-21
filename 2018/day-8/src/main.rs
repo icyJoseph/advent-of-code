7d38791f5fc50506ea050ec2175d0d3e0686f9ea
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-08-21 21:05:24 +0200

    feat: Solve Day 8 2018, part 1

---
+++ 2018/day-8/src/main.rs
@@ -0,0 +1,70 @@
+use aoc;
+
+#[derive(Debug)]
+struct Node {
+    children: Vec<Node>,
+    metadata: Vec<usize>,
+}
+
+impl Node {
+    fn new<'a, I>(it: &mut I) -> Self
+    where
+        I: Iterator<Item = &'a usize>,
+    {
+        let children_len = match it.next() {
+            Some(&n) => n,
+            None => panic!("broken children instructions"),
+        };
+
+        let metadata_len = match it.next() {
+            Some(&n) => n,
+            None => panic!("broken meta instructions"),
+        };
+
+        let mut children = vec![];
+
+        for _ in 0..children_len {
+            children.push(Node::new(it));
+        }
+
+        let mut metadata = vec![];
+
+        for _ in 0..metadata_len {
+            match it.next() {
+                Some(&n) => metadata.push(n),
+                None => panic!("meta data length is broken"),
+            }
+        }
+
+        Node { children, metadata }
+    }
+
+    fn total_metadata(&self) -> usize {
+        let mut sum = self.metadata.iter().sum::<usize>();
+
+        for child in self.children.iter() {
+            sum += child.total_metadata();
+        }
+
+        sum
+    }
+}
+
+fn solve(raw: String) -> () {
+    let input = raw
+        .trim()
+        .split(" ")
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    let mut it = input.iter();
+
+    let root = Node::new(&mut it);
+
+    println!("Part 1: {}", root.total_metadata());
+}
+
+fn main() {
+    let input = aoc::get_input(2018, 8);
+    solve(input);
+}
