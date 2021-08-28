b71897f4bb80139e69ae8f2cf65c6fa0b66ff55d
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-08-29 01:50:43 +0200

    feat: Solve Day 14 2018, part 2

--- 2018/day-14/src/main.rs
+++ 2018/day-14/src/main.rs
@@ -24,7 +24,7 @@ fn break_to_digits() {
     assert_eq!(to_digits(8), [8]);
 }
 
-fn solve(base: usize) -> () {
+fn part_one(base: usize) -> () {
     let mut recipes = vec![3, 7, 1, 0];
 
     let mut a = 0usize;
@@ -58,7 +58,50 @@ fn solve(base: usize) -> () {
     );
 }
 
+fn includes(src: &Vec<usize>, target: &Vec<usize>) -> Option<usize> {
+    let window = target.len();
+
+    let lower_b = src.len() - 2 * window;
+
+    for i in lower_b..src.len() - window {
+        if &src[i..i + window] == target {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn part_two(base: usize) -> () {
+    let mut recipes = vec![3, 7, 1, 0];
+
+    let mut a = 0usize;
+    let mut b = 1usize;
+
+    loop {
+        let c_a = recipes[a];
+        let c_b = recipes[b];
+
+        for d in to_digits(c_a + c_b) {
+            recipes.push(d);
+        }
+
+        a = (a + 1 + c_a) % recipes.len();
+        b = (b + 1 + c_b) % recipes.len();
+
+        if recipes.len() > 2 * to_digits(base).len() {
+            match includes(&recipes, &to_digits(base)) {
+                Some(index) => {
+                    println!("Part 2: {}", index);
+                    break;
+                }
+                None => continue,
+            }
+        }
+    }
+}
+
 fn main() {
     let input = 909441;
-    solve(input);
+    part_one(input);
+    part_two(input);
 }
