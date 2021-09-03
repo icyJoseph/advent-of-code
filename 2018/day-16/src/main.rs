e0218d57266e473b1b40e6800f066620bfbbdc30
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-09-03 18:19:25 +0200

    feat: Solve Day 16 2018, part 2

--- 2018/day-16/src/main.rs
+++ 2018/day-16/src/main.rs
@@ -3,7 +3,7 @@ use aoc;
 use self::OpCode::*;
 use std::slice::Iter;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum OpCode {
     Addr,
     Addi,
@@ -99,8 +99,8 @@ impl OpCode {
     }
 }
 
-fn test(before: &[usize], instruction: &[usize], after: &[usize]) -> usize {
-    let mut total_matches = 0;
+fn test(before: &[usize], instruction: &[usize], after: &[usize]) -> (usize, Vec<OpCode>) {
+    let mut total_matches = (0, vec![]);
 
     let input_a = instruction[1];
     let input_b = instruction[2];
@@ -108,7 +108,8 @@ fn test(before: &[usize], instruction: &[usize], after: &[usize]) -> usize {
 
     for code in OpCode::iterator() {
         if after == code.execute(input_a, input_b, output, before) {
-            total_matches += 1;
+            total_matches.0 += 1;
+            total_matches.1.push(*code);
         }
     }
 
@@ -122,6 +123,9 @@ fn solve(raw: String) -> () {
 
     let samples = samples_group.split("\n\n").collect::<Vec<&str>>();
 
+    use std::collections::HashMap;
+
+    let mut freq: HashMap<usize, Vec<OpCode>> = HashMap::new();
     let mut over_three_matches = 0;
 
     for sample in samples {
@@ -132,13 +136,9 @@ fn solve(raw: String) -> () {
             .replace("[", "")
             .replace("]", "")
             .replace(",", "")
+            .trim()
             .split(" ")
-            .map(|n| n.parse::<usize>())
-            .filter(|n| if let Ok(_) = n { true } else { false })
-            .map(|n| match n {
-                Ok(i) => i,
-                Err(why) => panic!("{:?}", why),
-            })
+            .map(|n| n.parse::<usize>().unwrap())
             .collect::<Vec<_>>();
 
         let after = spec[2]
@@ -146,31 +146,83 @@ fn solve(raw: String) -> () {
             .replace("[", "")
             .replace("]", "")
             .replace(",", "")
+            .trim()
             .split(" ")
-            .map(|n| n.parse::<usize>())
-            .filter(|n| if let Ok(_) = n { true } else { false })
-            .map(|n| match n {
-                Ok(i) => i,
-                Err(why) => panic!("{:?}", why),
-            })
+            .map(|n| n.parse::<usize>().unwrap())
             .collect::<Vec<_>>();
 
         let instruction = spec[1]
+            .trim()
             .split(" ")
-            .map(|n| n.parse::<usize>())
-            .filter(|n| if let Ok(_) = n { true } else { false })
-            .map(|n| match n {
-                Ok(i) => i,
-                Err(why) => panic!("{:?}", why),
-            })
+            .map(|n| n.parse::<usize>().unwrap())
             .collect::<Vec<_>>();
 
-        if test(&before, &instruction, &after) >= 3 {
+        let (total_matches, codes) = test(&before, &instruction, &after);
+
+        if total_matches >= 3 {
             over_three_matches += 1;
         }
+
+        freq.insert(instruction[0], codes);
     }
 
     println!("Part 1: {}", over_three_matches);
+
+    let mut dictionary: HashMap<usize, OpCode> = HashMap::new();
+    let mut done = vec![];
+
+    loop {
+        for (key, value) in freq.iter_mut() {
+            if value.len() == 1 {
+                if let Some(code) = value.pop() {
+                    done.push(code);
+
+                    dictionary.insert(*key, code);
+                }
+            } else {
+                let mut new_value = vec![];
+
+                for v in value.iter() {
+                    if !done.contains(v) {
+                        new_value.push(*v);
+                    }
+                }
+
+                *value = new_value;
+            }
+        }
+
+        if done.len() == 16 {
+            break;
+        }
+    }
+
+    let program = groups[1]
+        .trim()
+        .split("\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|r| {
+            r.split(" ")
+                .map(|i| i.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<Vec<usize>>>();
+
+    let mut register = vec![0, 0, 0, 0];
+
+    for instruction in program {
+        let op_code = instruction[0];
+        let input_a = instruction[1];
+        let input_b = instruction[2];
+        let output = instruction[3];
+
+        if let Some(code) = dictionary.get(&op_code) {
+            register = code.execute(input_a, input_b, output, &register)
+        }
+    }
+
+    println!("Part 2: {}", register[0]);
 }
 
 fn main() {
