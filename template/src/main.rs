54a4b3fd88ceb9fa08945c1f7d6805b0c8386d7d
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-12-01 12:50:06 +0100

    template: Include ability to read from file system

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,10 +1,20 @@
 use aoc;
 
+fn parse_num<T: std::str::FromStr>(str: &str) -> T {
+    match str.trim().parse::<T>() {
+        Ok(n) => n,
+        _ => panic!("Error parsing"),
+    }
+}
+
 fn solve(raw: String) -> () {
     println!("{}", raw);
 }
 
 fn main() {
     let input = aoc::get_input({{year}}, {{day}});
+    
+    // let example_input = std::fs::read_to_string("").expect("Error reading input");
+
     solve(input);
 }
