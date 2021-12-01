22a2ff7874ecab23b094f93f35d6c0053b340b24
Joseph Chamochumbi <sephxd1234@gmail.com>
2021-12-01 06:44:05 +0100

    feat: Day 1 2021, two stars

--- 2021/day-1/src/main.rs
+++ 2021/day-1/src/main.rs
@@ -1,14 +1,47 @@
 use aoc;
 
+fn parse_num<T: std::str::FromStr>(str: &str) -> T {
+    match str.trim().parse::<T>() {
+        Ok(n) => n,
+        _ => panic!("Error parsing"),
+    }
+}
+
 fn solve(raw: String) -> () {
-    println!("{}", raw);
+    let rows = raw
+        .trim()
+        .split("\n")
+        .map(|x| parse_num::<u32>(x))
+        .collect::<Vec<u32>>();
+
+    let mut part_one_deltas = 0;
+
+    for (index, value) in rows[1..].iter().enumerate() {
+        let actual_index = index + 1;
+        if *value > rows[actual_index - 1] {
+            part_one_deltas += 1;
+        }
+    }
+    println!("Part one: {}", part_one_deltas); // 1226
+
+    let mut part_two_deltas = 0;
+    for (index, _) in rows[1..rows.len() - 2].iter().enumerate() {
+        let actual_index = index + 1;
+        let prev_window = rows[actual_index - 1..actual_index + 2].iter().sum::<u32>();
+        let next_window = rows[actual_index..actual_index + 3].iter().sum::<u32>();
+
+        if next_window > prev_window {
+            part_two_deltas += 1;
+        }
+    }
+    println!("Part two: {}", part_two_deltas); // 1252
 }
 
 fn main() {
     let input = aoc::get_input(2021, 1);
 
-    let raw_input = std::fs::read_to_string("./input/day-1.in").expect("Error reading input");
+    // let example_input = std::fs::read_to_string("./input/day-1.in").expect("Error reading input");
 
     solve(input);
-    solve(raw_input);
+    // solve(example_input);
 }
--- 2021/deno/day-1.ts
+++ 2021/deno/day-1.ts
@@ -1,12 +1,39 @@
 const input = await Deno.readTextFile("./input/day-1.in");
 
+const sum = (a: number, b: number) => a + b;
+
+const data = input.split("\n").map(Number);
+
 /**
  * Part One
  */
 
-console.log("Part One:", input);
+const deltas = data
+  .reduce((prev: number[], curr, index, src) => {
+    if (index === 0) return prev;
+    return [...prev, curr > src[index - 1] ? 1 : 0];
+  }, [])
+  .filter((x: number) => x).length;
+
+console.log("Part One:", deltas);
 
 /**
  * Part Two
  */
-console.log("Part Two:", input);
+
+const windowDeltas = data
+  .reduce((prev: number[], _, index, src) => {
+    if (index === 0) return prev;
+    if (src.length - index < 3) return prev;
+
+    const prevWindow = src.slice(index - 1, index + 2);
+    const nextWindow = src.slice(index, index + 3);
+
+    const prevSum = prevWindow.reduce(sum);
+    const nextSum = nextWindow.reduce(sum);
+
+    return [...prev, nextSum > prevSum ? 1 : 0];
+  }, [])
+  .filter((x: number) => x).length;
+
+console.log("Part One:", windowDeltas);
