f0a1ef1e36e975228b644b93ad4a663923ccd600
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-10-12 17:25:18 +0200

    refactor: Simplify day-1 with Swift

--- 2021/swift/day-1.swift
+++ 2021/swift/day-1.swift
@@ -5,34 +5,25 @@ let filename = "../input/day-1.in"
 func main() {
     do {
         let contents = try String(contentsOfFile: filename)
+
         let data = contents.split(separator: "\n").map {
             (val: String.SubSequence) -> Int in
             Int(val)!
         }
 
-        let countTrue: ([Bool]) -> Int = {
-            list in
-            list.filter { $0 }.count
-        }
-
         func solver(size: Int, list: [Int]) -> Int {
-            let slider: ([Bool], (Int, Int)) -> [Bool] = {
-                acc, curr -> [Bool] in
+            let slider: (Int, (Int, Int)) -> Int = {
+                acc, curr -> Int in
                 let (index, value) = curr
 
                 if index <= size {
                     return acc
                 }
-                var next = [Bool]()
 
-                next.append(contentsOf: acc)
-
-                next.append(value > list[index - size])
-
-                return next
+                return acc + (value > list[index - size] ? 1 : 0)
             }
 
-            return countTrue(list.enumerated().reduce([], slider))
+            return list.enumerated().reduce(0, slider)
         }
 
         print("Part one: \(solver(size: 1, list: data))")
