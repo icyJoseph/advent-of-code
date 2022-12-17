f3d7b95d244029f78323de7b3d65090065d16397
Joseph Chamochumbi <sephxd1234@gmail.com>
2022-12-17 07:24:13 +0100

    feat: AoC 2022 day 17, part one w/ TS

---
+++ 2022/deno/17.ts
@@ -0,0 +1,279 @@
+const filename = "17";
+
+const solve = async (example = false) => {
+  const input = await Deno.readTextFile(
+    `./input/${example ? "example" : filename}.in`
+  );
+
+  if (example) {
+    console.log("Example");
+  }
+
+  /*
+
+####
+
+.#.
+###
+.#.
+
+..#
+..#
+###
+
+#
+#
+#
+#
+
+##
+##
+
+  */
+
+  type RockType = "-" | "+" | "inverted-L" | "|" | "square";
+
+  const createRock = (type: RockType, bottom: number) => {
+    const self = { left: 2, bottom, settled: false };
+
+    function move(delta: { left: number; down: number }) {
+      self.left = self.left + delta.left;
+      self.bottom = self.bottom + delta.down;
+    }
+
+    function isSettled() {
+      return self.settled;
+    }
+
+    function setSettled() {
+      self.settled = true;
+    }
+
+    switch (type) {
+      case "-":
+        return {
+          type,
+          get self() {
+            return { ...self };
+          },
+          get coords() {
+            return [
+              [self.left, self.bottom],
+              [self.left + 1, self.bottom],
+              [self.left + 2, self.bottom],
+              [self.left + 3, self.bottom],
+            ];
+          },
+          get top() {
+            return self.bottom;
+          },
+          move,
+          isSettled,
+          setSettled,
+        };
+      case "+":
+        return {
+          type,
+          get self() {
+            return { ...self };
+          },
+          get coords() {
+            return [
+              [self.left + 1, self.bottom + 2],
+              [self.left, self.bottom + 1],
+              [self.left + 1, self.bottom + 1],
+              [self.left + 2, self.bottom + 1],
+              [self.left + 1, self.bottom],
+            ];
+          },
+          get top() {
+            return self.bottom + 2;
+          },
+          move,
+          isSettled,
+          setSettled,
+        };
+      case "inverted-L":
+        return {
+          type,
+          get self() {
+            return { ...self };
+          },
+          get coords() {
+            return [
+              [self.left + 2, self.bottom + 2],
+              [self.left + 2, self.bottom + 1],
+              [self.left, self.bottom],
+              [self.left + 1, self.bottom],
+              [self.left + 2, self.bottom],
+            ];
+          },
+          get top() {
+            return self.bottom + 2;
+          },
+          move,
+          isSettled,
+          setSettled,
+        };
+      case "|":
+        return {
+          type,
+          get self() {
+            return { ...self };
+          },
+          get coords() {
+            return [
+              [self.left, self.bottom + 3],
+              [self.left, self.bottom + 2],
+              [self.left, self.bottom + 1],
+              [self.left, self.bottom],
+            ];
+          },
+          get top() {
+            return self.bottom + 3;
+          },
+          move,
+          isSettled,
+          setSettled,
+        };
+      case "square":
+        return {
+          type,
+          get self() {
+            return { ...self };
+          },
+          get coords() {
+            return [
+              [self.left, self.bottom + 1],
+              [self.left + 1, self.bottom + 1],
+              [self.left, self.bottom],
+              [self.left + 1, self.bottom],
+            ];
+          },
+          get top() {
+            return self.bottom + 1;
+          },
+          move,
+          isSettled,
+          setSettled,
+        };
+    }
+  };
+
+  const rockCycle: Readonly<RockType[]> = Object.freeze([
+    "-",
+    "+",
+    "inverted-L",
+    "|",
+    "square",
+  ]);
+  const jetsCycle = input.split("");
+
+  const makeIterator = <T>(list: Readonly<T[]>) => {
+    let index = 0;
+
+    return {
+      next() {
+        const value = list[index];
+        index = (index + 1) % list.length;
+        return value;
+      },
+    };
+  };
+
+  const width = 7; // |..@@@@.|
+  const floor = 0;
+  const gap = 3 + 1;
+
+  const rockTypeIt = makeIterator(rockCycle);
+  const jetsIt = makeIterator(jetsCycle);
+
+  let current = createRock(rockTypeIt.next(), floor + gap);
+
+  const filled = new Set<number>();
+
+  type Rock = ReturnType<typeof createRock>;
+  const settled: Rock[] = [];
+
+  const limit = 2022;
+
+  while (settled.length < limit) {
+    // console.log(settled.length);
+    // first move with the jets
+    const jet = jetsIt.next();
+
+    // console.log("\nBefore jets", jet, current.self, current.type);
+
+    if (jet === "<") {
+      current.move({ left: -1, down: 0 });
+    } else {
+      // ">"
+      current.move({ left: 1, down: 0 });
+    }
+
+    const hitWalls = current.coords.some(([x, _]) => x < 0 || x >= width);
+    // the x movement caused it to touch filled
+    const hitsFilledX = current.coords.some(([x, y]) =>
+      filled.has(y * width + x)
+    );
+
+    // undo
+    if (hitWalls || hitsFilledX) {
+      if (jet === "<") {
+        current.move({ left: 1, down: 0 });
+      } else {
+        // ">"
+        current.move({ left: -1, down: 0 });
+      }
+    }
+
+    // console.log("After jets", jet, current.self, current.type);
+
+    // then fall
+    current.move({ left: 0, down: -1 });
+
+    // check that no collisions exist
+    const hitsFloor = current.coords.some(([_, y]) => y === 0);
+    const hitsFilled = current.coords.some(([x, y]) =>
+      filled.has(y * width + x)
+    );
+
+    // console.log({ hitsFloor, hitsFilled });
+    if (hitsFloor || hitsFilled) {
+      // undo
+      current.move({ left: 0, down: 1 });
+      // settle
+      current.setSettled();
+    }
+    // console.log("After gravity", current.self, current.type);
+
+    if (current.isSettled()) {
+      current.coords.forEach(([x, y]) => {
+        filled.add(y * width + x);
+      });
+
+      settled.push(current);
+
+      const top = settled.map((rock) => rock.top).sort((a, b) => b - a);
+
+      current = createRock(rockTypeIt.next(), top[0] + gap);
+    }
+  }
+
+  /**
+   * Part One
+   */
+
+  console.log(
+    "Part one:",
+    settled.map((rock) => rock.top).sort((a, b) => b - a)[0]
+  );
+
+  /**
+   * Part Two
+   */
+};
+
+await solve(true);
+console.log("---");
+await solve();
