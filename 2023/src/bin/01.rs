f15624c8c2f53f34638a8cb439d5ea9da90b84e5
Joseph Chamochumbi <sephxd1234@gmail.com>
2024-01-29 14:35:00 +0100

    feat: AoC 2023 Day 1, w/ Rust in constant space

--- 2023/src/bin/01.rs
+++ 2023/src/bin/01.rs
@@ -5,16 +5,21 @@ fn main(input: &str) -> (u32, u32) {
     let mut part_one = 0;
 
     for line in lines {
-        let digits = line
-            .chars()
-            .filter_map(|c| c.to_digit(10))
-            .collect::<Vec<_>>();
+        let mut first: Option<u32> = None;
+        let mut last: Option<u32> = None;
 
-        let Some (first) = digits.first() else {
+        line.chars().for_each(|c| {
+            if let Some(digit) = c.to_digit(10) {
+                first.get_or_insert(digit);
+                last.replace(digit);
+            }
+        });
+
+        let Some (first) = first else {
             panic!("No first digit");
         };
 
-        let Some (last) = digits.last() else {
+        let Some (last) = last else {
             panic!("No last digit");
         };
 
@@ -30,36 +35,41 @@ fn main(input: &str) -> (u32, u32) {
     let mut part_two = 0;
 
     for line in input.lines() {
-        let mut digits = vec![];
+        let mut first: Option<u32> = None;
+        let mut last: Option<u32> = None;
 
-        for (i, ch) in line.chars().enumerate() {
-            match ch.to_digit(10) {
-                Some(digit) => digits.push(digit),
+        line.chars()
+            .enumerate()
+            .for_each(|(i, ch)| match ch.to_digit(10) {
+                Some(digit) => {
+                    first.get_or_insert(digit);
+                    last.replace(digit);
+                }
                 _ => {
-                    for (digit, word) in &words {
+                    for &(digit, word) in &words {
                         let upper = i + word.len();
 
                         if line.len() < upper {
                             continue;
                         }
 
-                        if *word != &line[i..upper] {
+                        if word != &line[i..upper] {
                             continue;
                         }
 
-                        digits.push(*digit);
+                        first.get_or_insert(digit);
+                        last.replace(digit);
 
                         break;
                     }
                 }
-            }
-        }
+            });
 
-        let Some (first) = digits.first() else {
+        let Some (first) = first else {
             panic!("No first digit");
         };
 
-        let Some (last) = digits.last() else {
+        let Some (last) = last else {
             panic!("No last digit");
         };
 
