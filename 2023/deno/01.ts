dd12ba965e90bd83a407035b9645d7dd1535ee03
Joseph Chamochumbi <sephxd1234@gmail.com>
2024-03-10 21:49:52 +0100

    refactor: AoC 2023 Day 1, w/ TS @ ~23ms

--- 2023/deno/01.ts
+++ 2023/deno/01.ts
@@ -6,25 +6,6 @@ const sum = (a: number, b: number) => a + b;
 
 const isNumber = (n: number) => !isNaN(n);
 
-const indexOfAll = (list: string, search: string) => {
-  let pos = 0;
-
-  const chunks: Array<[string, number]> = [];
-
-  while (pos < list.length) {
-    const index = list.indexOf(search, pos);
-
-    if (index === -1) {
-      break;
-    }
-    chunks.push([search, index]);
-
-    pos = pos + 1;
-  }
-
-  return chunks;
-};
-
 async function solve(path: string) {
   const input = await Deno.readTextFile(path);
 
@@ -49,35 +30,42 @@ async function solve(path: string) {
    * Part Two
    */
 
-  const words =
+  const wordRef =
     "one, two, three, four, five, six, seven, eight, nine".split(
-      ", "
+      ", ",
     );
 
-  const allVariations = words
-    .map((w, d) => [w, `${d + 1}`])
-    .flat();
-
-  const numberDict = words.reduce<Record<string, number>>(
-    (acc, curr, pos) => {
-      return { ...acc, [curr]: pos + 1 };
-    },
-    {}
-  );
+  const words = wordRef.map((w, d) => [d + 1, w] as const);
 
   const second = input
     .split("\n")
     .map((chunk) => {
-      const numbers = allVariations
-        .map((variation) => indexOfAll(chunk, variation))
-        .flat()
-        .sort((a, b) => a[1] - b[1])
-        .map(([w]) => numberDict[w] ?? Number(w));
+      let first = 0;
+      let last = 0;
 
-      const f = numbers[0];
-      const s = numbers[numbers.length - 1] ?? f;
+      for (let index = 0; index < chunk.length; index++) {
+        const ch = chunk[index];
+        const digit = Number(ch);
 
-      return 10 * f + s;
+        if (isNumber(digit)) {
+          first = first || digit;
+          last = digit;
+
+          continue;
+        }
+
+        words.find(([digit, word]) => {
+          if (
+            chunk.slice(index, index + word.length) === word
+          ) {
+            first = first || digit;
+            last = digit;
+            return;
+          }
+        });
+      }
+
+      return 10 * first + last;
     })
     .reduce(sum);
 
