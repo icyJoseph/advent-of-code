b3aeb5eeb2b4093430e07cb284f0248257067b22
Joseph Chamochumbi <sephxd1234@gmail.com>
2024-11-04 21:56:54 +0100

    feat: AoC 2023 Day 13, w/ Rust :star: :star: 8ms

--- 2023/src/bin/13.rs
+++ 2023/src/bin/13.rs
@@ -26,6 +26,11 @@ fn is_palindrome(line: &[char]) -> bool {
     true
 }
 
+enum Slice {
+    Row,
+    Col,
+}
+
 impl Mirror {
     fn new(desc: &str) -> Mirror {
         let data: Vec<_> = desc
@@ -66,49 +71,91 @@ impl Mirror {
         true
     }
 
-    fn find_col_reflection(&self, at: usize) -> Option<usize> {
-        if at == self.cols {
-            return None;
+    fn find_reflection_index(&self, at: usize, line: Slice, skip: Option<usize>) -> Option<usize> {
+        match skip {
+            Some(index) if at == index => {
+                // noop
+            }
+            _ => {
+                let total = match line {
+                    Slice::Row => self.rows,
+                    Slice::Col => self.cols,
+                };
+
+                if at == total {
+                    return None;
+                }
+
+                let tail_count = total - at - 1;
+                let head_count = at + 1;
+
+                let count = min(head_count, tail_count);
+
+                let from = head_count - count;
+                let to = at + count;
+
+                let result = match line {
+                    Slice::Row => self.slice_rows(from, to),
+                    Slice::Col => self.slice_cols(from, to),
+                };
+
+                if result {
+                    return Some(at);
+                }
+            }
         }
 
-        let tail_count = self.cols - at - 1;
-        let head_count = at + 1;
-
-        let count = min(head_count, tail_count);
-
-        if self.slice_cols(head_count - count, at + count) {
-            return Some(at);
-        }
-
-        self.find_col_reflection(at + 1)
+        self.find_reflection_index(at + 1, line, skip)
     }
 
-    fn find_row_reflection(&self, at: usize) -> Option<usize> {
-        if at == self.rows {
-            return None;
-        }
+    fn find_reflection(
+        &self,
+        skip_row: Option<usize>,
+        skip_col: Option<usize>,
+    ) -> (Option<usize>, Option<usize>) {
+        let row = self.find_reflection_index(0, Slice::Row, skip_row);
+        let col = self.find_reflection_index(0, Slice::Col, skip_col);
 
-        let tail_count = self.rows - at - 1;
-        let head_count = at + 1;
-
-        let count = min(head_count, tail_count);
-
-        if self.slice_rows(head_count - count, at + count) {
-            return Some(at);
-        }
-
-        self.find_row_reflection(at + 1)
+        (row, col)
     }
 
-    fn find_reflection(&self) -> usize {
-        let row = self.find_row_reflection(0);
-        let col = self.find_col_reflection(0);
+    fn score_reflection(&self) -> usize {
+        let (row, col) = self.find_reflection(None, None);
 
+        Mirror::score(row, col)
+    }
+
+    fn score(row: Option<usize>, col: Option<usize>) -> usize {
         match (row, col) {
             (Some(r), None) => (r + 1) * 100,
             (None, Some(c)) => c + 1,
-            _ => 0, //panic!("No reflection!"),
+            _ => panic!("No reflection!"),
         }
+    }
+
+    fn with_smudge(&mut self) -> usize {
+        let (row, col) = self.find_reflection(None, None);
+
+        for r in 0..self.rows {
+            for c in 0..self.cols {
+                let current = self.data[r][c];
+
+                self.data[r][c] = if current == '#' { '.' } else { '#' };
+
+                let (rr, cc) = self.find_reflection(row, col);
+
+                self.data[r][c] = current;
+
+                match (rr, cc) {
+                    (None, None) => {
+                        continue;
+                    }
+                    _ => return Mirror::score(rr, cc),
+                }
+            }
+        }
+
+        panic!("No new reflection!");
     }
 }
 
@@ -119,10 +166,20 @@ fn main(input: &str) -> (usize, usize) {
     for line in input.split("\n\n") {
         let mirror = Mirror::new(line);
 
-        let result = mirror.find_reflection();
+        let result = mirror.score_reflection();
 
         part_one += result;
     }
 
-    (part_one, 0)
+    let mut part_two = 0;
+
+    for line in input.split("\n\n") {
+        let mut mirror = Mirror::new(line);
+
+        let result = mirror.with_smudge();
+
+        part_two += result;
+    }
+
+    (part_one, part_two)
 }
